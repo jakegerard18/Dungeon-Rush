33036da2d4554bf9e11b8fb0946daa814823cb6d
Your Name <you@example.com>
2021-12-31 00:46:29 -0600

    Test push.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { spriteDecomposer } from './utils/spriteDecomposer';
 import Test_Dungeon from './scenes/TestDungeon'
 
 //console.log(spriteDecomposer('troll_flipped.png', 192, 538, 48, 11, 4));
-
+//TEST
 export const game = new Phaser.Game({
 	type: Phaser.AUTO,
 	width: 800,
