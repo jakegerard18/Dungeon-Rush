f2fa44f44c2ac03ea0766fb51c9e540ed91f8078
Jake Gerard <jakegerard18@gmail.com>
2022-02-12 00:29:20 -0600

    Add map util class and room changes.

--- src/utils/Map.ts
+++ src/utils/Map.ts
@@ -1,4 +1,13 @@
 import Phaser from 'phaser'
+
+enum Directions
+{
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+}
+
 export default class Map
 {
   scene: Phaser.Scene;
@@ -16,14 +25,37 @@ export default class Map
       this.rooms.push(room);
     })
 
-    let i = 0;
+    var x = 0;
+    var y = 0;
     this.rooms.forEach((room) => {
       let tileset = room.addTilesetImage('dungeon_tileset', 'tiles', 16, 16, 1, 2);
-      room.createLayer('layer', tileset, i, 0);
-      i += 256;
+      const layer = room.createLayer('layer', tileset, x, y);
+      layer.setCollisionByProperty({collides: true});
+
+      switch(this.randomDirection()) {
+        case Directions.NORTH:
+          y -= 256;
+          break;
+
+        case Directions.EAST:
+          x += 256;
+          break;
+
+        case Directions.SOUTH:
+          y += 256;
+          break;
+
+        case Directions.WEST:
+          x -= 256;
+          break;
+      }
     })
   }
 
+  randomDirection() {
+    var newDirection = Phaser.Math.Between(0, 3);
+    return newDirection
+  }
 
 
 }
