eedc2ddb476f6080cffb6e0e863f8c85c2dc8345
Jake Gerard <jakegerard@gmail.com>
2022-06-28 23:20:29 -0500

    Change stuff

--- src/scenes/TestDungeon.ts
+++ src/scenes/TestDungeon.ts
@@ -1,97 +1,33 @@
 import Phaser from 'phaser'
-import Hero from '../characters/Hero'
-import Slime from '../characters/Slime'
-import Rat from '../characters/Rat'
-import Bat from '../characters/Bat'
-import Spider from '../characters/Spider'
-import Goblin from '../characters/Goblin'
-import Archer from '../characters/Archer'
-import Orc from '../characters/Orc'
-import Troll from '../characters/Troll'
-import Controls from '../utils/Controls'
-import Map from '../utils/Map'
-import { createBatAnims, createHeroAnims, createRatAnims, createSlimeAnims, createSpiderAnims, createGoblinAnims, createArcherAnims, createOrcAnims, createTrollAnims } from '../animations/Animations'
-import { handleHeroRatCollision, handleHeroSlimeCollision, handleSwordRatCollision, handleSwordSlimeCollision, handleDoorCollision } from '../colliders/Collision_Callbacks'
-import { showCollisions } from '../utils/ShowCollisions'
-import { controlCamera } from '../utils/ControlCamera'
-import { game } from '../main'
+
 
 
 export default class Test_Dungeon extends Phaser.Scene {
-  private map: Map;
-  private cursors: Controls;
-  private hero: Hero;
-  private slime: Slime;
-  private rat: Rat;
-  private bat: Bat;
-  private spider: Spider;
-  private goblin: Goblin;
-  private archer: Archer;
-  private orc: Orc;
-  private troll: Troll;
-  private enemies: Phaser.Physics.Arcade.Sprite[];
-
+  var startText;
+  var started = false;
+  var placedSpecial;
+  var images = [];
+  var floorplan;
+  var floorplanCount;
+  var cellQueue;
+  var endrooms;
+  var maxrooms = 15;
+  var minrooms = 7;
+  var bossl; 
 
   constructor() {
     super('testdungeon');
   }
 
   preload() {
-    createHeroAnims(this.anims);
-    createSlimeAnims(this.anims);
-    createRatAnims(this.anims);
-    createBatAnims(this.anims);
-    createSpiderAnims(this.anims);
-    createGoblinAnims(this.anims);
-    createArcherAnims(this.anims);
-    createOrcAnims(this.anims);
-    createTrollAnims(this.anims);
+
   }
 
   create() {
+    startText = this.add.text(W/2, H/2, 'Click to Start', { fontSize: '32px', fill: '#FFF' });
+    startText.x -= startText.width / 2;
 
-    //Instantiate sprites
-    this.hero = new Hero(this, 300, 100, 'hero');
-
-    this.enemies = [
-      // this.bat = new Bat(this, 100, 100, 'bat'),
-      this.spider = new Spider(this, 100, 100, 'spider'),
-        // this.goblin = new Goblin(this, 200, 200, 'goblin'),
-        // this.archer = new Archer(this, 200, 200, 'archer'),
-        // this.orc = new Orc(this, 300, 300, 'orc'),
-        this.troll = new Troll(this, 400, 100, 'troll'),
-        this.slime = new Slime(this, 100, 100, 'slime'),
-        this.rat = new Rat(this, 200, 200, 'rat')
-    ]
-
-    //Set cursor keys
-    this.cursors = new Controls(this, 'W', 'S', 'A', 'D', 'UP', 'DOWN', 'RIGHT', 'LEFT');
-
-    //Generate tilemap and floor layer
-    let rooms_json = ['room_1_json', 'room_2_json', 'room_3_json', 'room_4_json']
-    this.map = new Map(this, rooms_json);
-    this.map.generateMap()
-
-    //Add sprites to scenes
-    this.add.existing(this.hero);
-    this.enemies.forEach((enemy) => {
-      this.add.existing(enemy);
-    })
-
-    //const wallLayer = map.createLayer('Walls', tileset, 0, 0);
-    //Set colliders
-    //wallLayer.setCollisionByProperty({collides: true});
-
-    //Add colliders
-    // this.physics.add.collider(this.hero, wallLayer);
-    // this.physics.add.collider(this.enemies, wallLayer);
-    // this.physics.add.collider(this.hero, this.enemies, handleHeroRatCollision, undefined, this);
-    // this.physics.add.collider(this.hero.getSwordBody(this), this.enemies, handleSwordRatCollision, undefined, this);
-
-
-    //Set camera to follow hero sprite
-    this.cameras.main.startFollow(this.hero, true)
-    //showCollisions(wallLayer, this)
+    this.input.on('pointerdown', start, this);
   }
 
 
@@ -99,4 +35,5 @@ export default class Test_Dungeon extends Phaser.Scene {
     this.hero.update(this.cursors);
     //controlCamera(this, this.cursors);
   }
+
 }
--- src/utils/Map.ts
+++ src/utils/Map.ts
@@ -1,29 +1,191 @@
 import Phaser from 'phaser'
 export default class Map
+var cellw = 50;
+var cellh = 44;
+var W = 500;
+var H = 330;
+
+var config = {
+    type: Phaser.AUTO,
+    width: W,
+    height: H,
+    parent: 'isaac_gen',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
 {
-  scene: Phaser.Scene;
-  rooms_json: string[];
-  rooms = [];
+    if(window.baseUrl)
+        this.load.setBaseURL(baseUrl);
 
-  constructor(scene: Phaser.Scene, rooms_json: string[]) {
-    this.scene = scene;
-    this.rooms_json = rooms_json;
-  }
+    this.load.image('cell', 'cell.png');
+    this.load.image('boss', 'boss.png');
+    this.load.image('reward', 'reward.png');
+    this.load.image('coin', 'coin.png');
+    this.load.image('secret', 'secret.png');
+}
 
-  generateMap() {
-    this.rooms_json.forEach((room_json) => {
-      let room = this.scene.make.tilemap({ key: room_json });
-      this.rooms.push(room);
-    })
+var startText;
+var started = false;
+var placedSpecial;
+var images = [];
+var floorplan;
+var floorplanCount;
+var cellQueue;
+var endrooms;
+var maxrooms = 15;
+var minrooms = 7;
+var bossl;
 
-    let i = 0;
-    this.rooms.forEach((room) => {
-      let tileset = room.addTilesetImage('dungeon_tileset', 'tiles', 16, 16, 1, 2);
-      room.createLayer('layer', tileset, i, 0);
-      i += 256;
-    })
-  }
+function create ()
+{
+    startText = this.add.text(W/2, H/2, 'Click to Start', { fontSize: '32px', fill: '#FFF' });
+    startText.x -= startText.width / 2;
+
+    this.input.on('pointerdown', start, this);
+}
+
+function img(scene, i, name)
+{
+    var x = i % 10;
+    var y = (i - x) / 10;
+    var img = scene.add.image(W/2 + cellw * (x - 5), H/2 + cellh * (y - 4), name);
+    images.push(img);
+    return img;
+}
+
+function poprandomendroom()
+{
+    var index = Math.floor(Math.random() * endrooms.length);
+    var i = endrooms[index];
+    endrooms.splice(index, 1);
+    return i;
+}
+
+function picksecretroom()
+{
+    for(var e=0;e<900;e++)
+    {
+        var x = Math.floor(Math.random() * 9) + 1;
+        var y = Math.floor(Math.random() * 8) + 2;
+        var i = y*10 + x;
+
+        if(floorplan[i])
+            continue;
+
+        if (bossl == i-1 || bossl == i+1 || bossl == i+10 || bossl == i-10)
+            continue;
+
+        if(ncount(i) >= 3)
+            return i;
+
+        if(e > 300 && ncount >= 2)
+            return i;
+
+        if(e > 600 && ncount >= 1)
+            return i;
+    }
+}
 
 
+function update()
+{
+    if(started){
+        if(cellQueue.length > 0)
+        {
+            var i = cellQueue.shift();
+            var x = i % 10;
+            var created = false;
+            if(x > 1) created = created | visit(this, i - 1);
+            if(x < 9) created = created | visit(this, i + 1);
+            if(i > 20) created = created | visit(this, i - 10);
+            if(i < 70) created = created | visit(this, i + 10);
+            if(!created) {
+                endrooms.push(i);
+            }
+        }
+        else if(!placedSpecial) {
 
+            if(floorplanCount < minrooms) {
+                start.apply(this);
+                return;
+            }
+
+            placedSpecial = true;
+            bossl = endrooms.pop();
+            var cellImage = img(this, bossl, 'boss');
+            cellImage.x += 1;
+            
+            var rewardl = poprandomendroom();
+            var cellImage = img(this, rewardl, 'reward');
+
+            var coinl = poprandomendroom();
+            img(this, coinl, 'coin');
+
+            var secretl = picksecretroom();
+            img(this, secretl, 'cell');
+            img(this, secretl, 'secret');
+
+            if (!rewardl || !coinl || !secretl) {
+                start.apply(this);
+                return;
+            }
+        }
+    }
+}
+
+function start()
+{
+    started = true;
+    placedSpecial = false;
+    if(startText) {
+        startText.destroy();
+        startText = null;
+    }
+    images.forEach(image => {
+        image.destroy();
+    });
+    images = [];
+    floorplan = [];
+    for(var i =0;i<=100;i++) floorplan[i] = 0;
+    floorplanCount = 0;
+    cellQueue = [];
+    endrooms = [];
+    // Our grid is one row lower than isaacs as referencing negative indices isn't a good idea in JS
+    visit(this, 45);
+}
+
+function ncount(i)
+{
+    return floorplan[i-10] + floorplan[i-1] + floorplan[i+1] + floorplan[i+10];
+}
+
+function visit(scene, i)
+{
+    if(floorplan[i])
+        return false;
+
+    var neighbours = ncount(i);
+
+    if (neighbours > 1)
+        return false;
+
+    if (floorplanCount >= maxrooms)
+        return false;
+
+    if(Math.random() < 0.5 && i != 45)
+        return false;
+
+    cellQueue.push(i);
+    floorplan[i] = 1;
+    floorplanCount += 1;
+
+    img(scene, i, 'cell')
+    return true;
 }
