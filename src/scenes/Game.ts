8cbcd90614ef1b1822df7c04ca7f586a3f98b609
jakegerard18 <jakegerard18@gmail.com>
2022-12-11 16:10:16 -0600

    Add basic hero animations.

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -10,6 +10,48 @@ export default class Game extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(0, 0, 'tiles');
+        const dungeon = this.make.tilemap({ key: 'n_dungeon' });
+        const tileset = dungeon.addTilesetImage('dungeon_tiles', 'tiles');
+
+        dungeon.createLayer('Floor', tileset);
+        const wallsLayer = dungeon.createLayer('Walls', tileset);
+
+        wallsLayer.setCollisionByProperty({collides: true});
+        const debugGraphics = this.add.graphics().setAlpha(0.75);
+        // wallsLayer.renderDebug(debugGraphics, {
+        //     tileColor: null,
+        //     collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+        //     faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        // });
+
+        const hero = this.add.sprite(128, 128, 'hero')
+
+        this.anims.create({
+            key: 'hero-idle',
+            frames: this.anims.generateFrameNames('hero', {prefix: 'hero-idle-', start: 0, end: 2 }),
+            yoyo: true,
+            repeat: -1,
+            frameRate: 5
+        })
+        this.anims.create({
+            key: 'hero-walk-down',
+            frames: this.anims.generateFrameNames('hero', {prefix: 'hero-walk-down-', start: 0, end: 3 }),
+            repeat: -1,
+            frameRate: 10
+        })
+        this.anims.create({
+            key: 'hero-walk-up',
+            frames: this.anims.generateFrameNames('hero', {prefix: 'hero-walk-up-', start: 0, end: 3 }),
+            repeat: -1,
+            frameRate: 10
+        })
+        this.anims.create({
+            key: 'hero-walk-right',
+            frames: this.anims.generateFrameNames('hero', {prefix: 'hero-walk-right-', start: 0, end: 3 }),
+            repeat: -1,
+            frameRate: 10
+        })
+
+        hero.anims.play('hero-idle')
     }
 }
