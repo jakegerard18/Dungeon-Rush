34d628ad114565fcba359e16dfd2361bbe59970f
jakegerard18 <jakegerard18@gmail.com>
2022-12-11 16:09:58 -0600

    Preload dungeon and hero.

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -6,7 +6,9 @@ export default class Preloader extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('tiles', 'tilemaps/tiles_dungeon_v1.png')
+        this.load.image('tiles', 'tilemaps/tiles_dungeon_v1.png');
+        this.load.tilemapTiledJSON('n_dungeon', 'dungeons/n_dungeon.json');
+        this.load.atlas('hero', 'sprites/packed/hero/hero.png', 'sprites/packed/hero/hero.json');
     }
 
     create() {
