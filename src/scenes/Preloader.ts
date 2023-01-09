9529cde4b6f5753275fc216ce0aa1daf2b51134d
Jake Gerard <jakegerard18@gmail.com>
2023-01-09 15:12:52 -0600

    Add slime atlas to preloader.

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -9,6 +9,7 @@ export default class Preloader extends Phaser.Scene {
         this.load.image('tiles', 'tilemaps/tiles_dungeon_v1.png');
         this.load.tilemapTiledJSON('n_dungeon', 'dungeons/n_dungeon.json');
         this.load.atlas('hero', 'sprites/packed/hero/hero.png', 'sprites/packed/hero/hero.json');
+        this.load.atlas('slime', 'sprites/packed/slime/slime.png', 'sprites/packed/slime/slime.json');
     }
 
     create() {
