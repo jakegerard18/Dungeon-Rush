682df4ad0ffa5a0417fe3a153828e1cb58785ded
Jake Gerard <jakegerard18@gmail.com>
2023-01-29 18:56:13 -0600

    Add createAnimations function.

--- src/Animations.ts
+++ src/Animations.ts
@@ -1,84 +1,40 @@
 import Phaser from 'phaser';
 
-// Hero animations
-
-export function createHeroAnims( scene: Phaser.Scene) {
+export function createAnimations(scene: Phaser.Scene, sprite: string) {
     scene.anims.create({
-        key: 'hero-idle',
-        frames: scene.anims.generateFrameNames('hero', {prefix: 'hero-idle-', start: 0, end: 2 }),
+        key: `${sprite}-idle`,
+        frames: scene.anims.generateFrameNames(sprite, {prefix: `${sprite}-idle-`, start: 0, end: 2 }),
         yoyo: true,
         repeat: -1,
         frameRate: 5
     });
     scene.anims.create({
-        key: 'hero-victory',
-        frames: scene.anims.generateFrameNames('hero', {prefix: 'hero-taunt-', start: 0, end: 2 }),
+        key: `${sprite}-taunt`,
+        frames: scene.anims.generateFrameNames(sprite, {prefix: `${sprite}-taunt-`, start: 0, end: 2 }),
         repeat: -1,
         frameRate: 5
     });
     scene.anims.create({
-        key: 'hero-walk-down',
-        frames: scene.anims.generateFrameNames('hero', {prefix: 'hero-walk-down-', start: 0, end: 3 }),
+        key: `${sprite}-walk-down`,
+        frames: scene.anims.generateFrameNames(sprite, {prefix: `${sprite}-walk-down-`, start: 0, end: 3 }),
         repeat: -1,
         frameRate: 10
     });
     scene.anims.create({
-        key: 'hero-walk-up',
-        frames: scene.anims.generateFrameNames('hero', {prefix: 'hero-walk-up-', start: 0, end: 3 }),
+        key: `${sprite}-walk-up`,
+        frames: scene.anims.generateFrameNames(sprite, {prefix: `${sprite}-walk-up-`, start: 0, end: 3 }),
         repeat: -1,
         frameRate: 10
     });
     scene.anims.create({
-        key: 'hero-walk-right',
-        frames: scene.anims.generateFrameNames('hero', {prefix: 'hero-walk-right-', start: 0, end: 3 }),
+        key: `${sprite}-walk-right`,
+        frames: scene.anims.generateFrameNames(sprite, {prefix: `${sprite}-walk-right-`, start: 0, end: 3 }),
         repeat: -1,
         frameRate: 10
     });
     scene.anims.create({
-        key: 'hero-walk-left',
-        frames: scene.anims.generateFrameNames('hero', {prefix: 'hero-walk-right-', start: 0, end: 3 }),
-        repeat: -1,
-        frameRate: 10
-    });
-}
-
-// Slime animations
-
-export function createSlimeAnims(scene: Phaser.Scene) {
-     scene.anims.create({
-        key: 'slime-idle',
-        frames: scene.anims.generateFrameNames('slime', {prefix: 'slime-idle-', start: 0, end: 2 }),
-        yoyo: true,
-        repeat: -1,
-        frameRate: 5
-    });
-    scene.anims.create({
-        key: 'slime-taunt',
-        frames: scene.anims.generateFrameNames('slime', {prefix: 'slime-taunt-', start: 0, end: 2 }),
-        repeat: -1,
-        frameRate: 5
-    });
-    scene.anims.create({
-        key: 'slime-walk-down',
-        frames: scene.anims.generateFrameNames('slime', {prefix: 'slime-walk-down-', start: 0, end: 3 }),
-        repeat: -1,
-        frameRate: 10
-    });
-    scene.anims.create({
-        key: 'slime-walk-up',
-        frames: scene.anims.generateFrameNames('slime', {prefix: 'slime-walk-up-', start: 0, end: 3 }),
-        repeat: -1,
-        frameRate: 10
-    });
-    scene.anims.create({
-        key: 'slime-walk-right',
-        frames: scene.anims.generateFrameNames('slime', {prefix: 'slime-walk-right-', start: 0, end: 3 }),
-        repeat: -1,
-        frameRate: 10
-    });
-    scene.anims.create({
-        key: 'slime-walk-left',
-        frames: scene.anims.generateFrameNames('slime', {prefix: 'slime-walk-right-', start: 0, end: 3 }),
+        key: `${sprite}-walk-left`,
+        frames: scene.anims.generateFrameNames(sprite, {prefix: `${sprite}-walk-right-`, start: 0, end: 3 }),
         repeat: -1,
         frameRate: 10
     });
