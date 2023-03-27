369c4227206341a9621f304a090becf9561dd94e
Jake Gerard <jakegerard18@gmail.com>
2023-03-27 00:13:01 -0500

    Add playerKeyCodes and PlayerKeys types.

--- src/Types.ts
+++ src/Types.ts
@@ -1,8 +1,31 @@
+import Phaser from "phaser"
 export namespace Types {
-    
-   export enum SpriteState {
+
+  export enum SpriteState {
         Attacking = 'attacking',
-        Vulnerable = 'Vulnerable'
+        Vulnerable = 'vulnerable'
+  }
+
+    export interface playerKeyCodes {
+        up: number,
+        down: number,
+        left: number,
+        right: number,
+        attackUp: number,
+        attackDown: number,
+        attackLeft: number,
+        attackRight: number
+    };
+
+    export interface PlayerKeys {
+      up: Phaser.Input.Keyboard.Key,
+      down: Phaser.Input.Keyboard.Key,
+      left: Phaser.Input.Keyboard.Key,
+      right: Phaser.Input.Keyboard.Key,
+      attackUp: Phaser.Input.Keyboard.Key,
+      attackDown: Phaser.Input.Keyboard.Key,
+      attackLeft: Phaser.Input.Keyboard.Key,
+      attackRight:  Phaser.Input.Keyboard.Key,
     }
 
    export interface MovementState {
