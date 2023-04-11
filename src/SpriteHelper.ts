4e40d84b3c60f1e32f521c01955355edc43ebc1a
Jake Gerard <jakegerard18@gmail.com>
2023-04-10 19:51:18 -0500

    Remove sprite helper

--- src/SpriteHelper.ts
+++
@@ -1,7 +0,0 @@
-import Phaser from 'phaser';
-
-export function addSprite(scene: Phaser.Scene, spriteKey: string, x: number, y: number) {
-   const sprite = scene.physics.add.sprite(x, y, spriteKey);
-   sprite.body.setSize(sprite.width * 0.3, sprite.height * 0.3);
-   return sprite;
-}
