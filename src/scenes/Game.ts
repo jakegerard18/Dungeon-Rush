1a380008ff2eccad877eff1648a76b33c28a1a7f
Jake Gerard <jakegerard18@gmail.com>
2023-04-10 22:56:02 -0500

    Fix indentation

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -12,57 +12,47 @@ export default class Game extends Phaser.Scene {
     private hero!: Hero.HeroClass;
     private keys;
     private keyCodes = {
-        up: Phaser.Input.Keyboard.KeyCodes.W,
-        down: Phaser.Input.Keyboard.KeyCodes.S,
-        left: Phaser.Input.Keyboard.KeyCodes.A,
-        right: Phaser.Input.Keyboard.KeyCodes.D,
-        attackUp: Phaser.Input.Keyboard.KeyCodes.UP,
-        attackDown: Phaser.Input.Keyboard.KeyCodes.DOWN,
-        attackLeft: Phaser.Input.Keyboard.KeyCodes.LEFT,
-        attackRight: Phaser.Input.Keyboard.KeyCodes.RIGHT
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+      attackUp: Phaser.Input.Keyboard.KeyCodes.UP,
+      attackDown: Phaser.Input.Keyboard.KeyCodes.DOWN,
+      attackLeft: Phaser.Input.Keyboard.KeyCodes.LEFT,
+      attackRight: Phaser.Input.Keyboard.KeyCodes.RIGHT
     };
 
     constructor() {
-        super('game');
+      super('game');
     }
 
     preload() {
-        this.keys = Keys.initKeys(this, this.keyCodes);
-        createAnimations(this, 'hero');
-        createAnimations(this, 'slime');
+      this.keys = Keys.initKeys(this, this.keyCodes);
+      createAnimations(this, 'hero');
+      createAnimations(this, 'slime');
     }
 
     create() {
-        this.scene.run('ui');
-        const dungeon = this.make.tilemap({ key: 'n_dungeon' });
-        const tileset = dungeon.addTilesetImage('dungeon_tiles', 'tiles');
-        const floorLayer = dungeon.createLayer('Floor', tileset);
-        const wallLayer = dungeon.createLayer('Walls', tileset);
+      this.scene.run('ui');
+      const dungeon = this.make.tilemap({ key: 'n_dungeon' });
+      const tileset = dungeon.addTilesetImage('dungeon_tiles', 'tiles');
+      const floorLayer = dungeon.createLayer('Floor', tileset);
+      const wallLayer = dungeon.createLayer('Walls', tileset);
 
-        this.hero = new Hero.HeroClass(this, 60, 100);
-        const slimes = this.physics.add.group({
-            classType: Slime,
-            createCallback: (go) => {
-                const slimeGo = go as Slime;
-                slimeGo.body.onCollide = true;
-                slimeGo.body.setSize(slimeGo.width * 0.3, slimeGo.height * 0.3);
-            }
-        })
-        slimes.get(80, 100, 'slime');
+      this.hero = new Hero.HeroClass(this, 60, 100);
+      let slime = new Slime(this, 100, 100);
 
-        wallLayer.setCollisionByProperty({collides: true});
+      wallLayer.setCollisionByProperty({collides: true});
 
-        this.physics.add.collider(this.hero, wallLayer);
-        this.cameras.main.startFollow(this.hero, true);
-        this.physics.add.collider(slimes, wallLayer);
-        this.physics.add.collider(this.hero, slimes, handleHeroSlimeCollision, undefined, this);
+      this.physics.add.collider(this.hero, wallLayer);
+      this.cameras.main.startFollow(this.hero, true);
+      this.physics.add.collider(slime, wallLayer);
+      this.physics.add.collider(this.hero, slime, handleHeroSlimeCollision, undefined, this);
     }
 
-
-
     update() {
-        if (this.hero) {
-            this.hero.update(this.keys);
-        }
+      if (this.hero) {
+          this.hero.update(this.keys);
+      }
     }
 }
