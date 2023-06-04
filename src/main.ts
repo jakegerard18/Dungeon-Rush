bc6a5c9a6a7b2edcd545433f1a52b63439f98a9e
Jake Gerard <jakegerard18@gmail.com>
2023-06-03 22:26:33 -0500

    Update width and height to test dungeon generation.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import Phaser from 'phaser'
-import Preloader from './scenes/Preloader'
-import Game from './scenes/Game'
+import Phaser from 'phaser';
+import Preloader from './scenes/Preloader';
+import Game from './scenes/Game';
 import UI from './scenes/UI';
+
 
 export default new Phaser.Game({
     type: Phaser.AUTO,
-    width: 800,
-    height: 500,
-    zoom: 2,
+    width: 2000,
+    height: 1000,
     physics: {
         default: 'arcade',
         arcade: {
