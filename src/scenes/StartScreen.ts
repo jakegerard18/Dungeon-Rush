bde76bf80694d79ba6672099080d025bc93aa2b5
Jake Gerard <jakegerard18@gmail.com>
2023-10-29 16:43:57 -0500

    Update start screen.

--- src/scenes/StartScreen.ts
+++ src/scenes/StartScreen.ts
@@ -8,10 +8,9 @@ export default class GameOver extends Phaser.Scene {
     create() {
       this.input.keyboard.addKeys(Phaser.Input.Keyboard.KeyCodes.SPACE);
       this.input.keyboard.on('keydown-SPACE', () => this.scene.start('game'), this);
-      this.add.text(this.cameras.main.centerX - 200, this.cameras.main.centerY - 100, "DUNGEON CLEANER", { fontSize: '32px', fill: '#AAFF00' });
-      this.add.text(this.cameras.main.centerX - 240, this.cameras.main.centerY - 50, "(A very silly roguelike)", { fontSize: '32px', fill: '#AAFF00' });
-      this.add.text(this.cameras.main.centerX - 420, this.cameras.main.centerY + 50, "The local dungeons are swarming with monsters,", { fontSize: '32px', fill: '#AAFF00' });
-      this.add.text(this.cameras.main.centerX - 420, this.cameras.main.centerY + 100, "and you've been called in to clean em' up.", { fontSize: '32px', fill: '#AAFF00' });
+      this.add.text(this.cameras.main.centerX - 150, this.cameras.main.centerY - 150, "DUNGEON RUSH", { fontSize: '32px', fill: '#AAFF00' });
+      this.add.text(this.cameras.main.centerX - 380, this.cameras.main.centerY - 50, "Clear as many dungeons as you can", { fontSize: '32px', fill: '#AAFF00' });
+      this.add.text(this.cameras.main.centerX - 380, this.cameras.main.centerY, "before the timer runs out!", { fontSize: '32px', fill: '#AAFF00' });
       this.add.text(this.cameras.main.centerX - 400, this.cameras.main.centerY + 150, "(Use wasd to move and arrow keys to attack)", { fontSize: '32px', fill: '#AAFF00' });
       this.add.text(this.cameras.main.centerX - 240, this.cameras.main.centerY + 200, "(Press spacebar to start)", { fontSize: '32px', fill: '#AAFF00' });
     }
